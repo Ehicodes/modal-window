fba5bca03545d143846b1f70b46aa5f5007a24a6
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-10 20:02:27 +0100

    using classList property and .remove method

--- starter/script.js
+++ starter/script.js
@@ -10,6 +10,16 @@ console.log(btnsOpenModal); //notice that it only selects one button and that is
 //we stored the selected elements in the variables. the NodeList is a little bit like an array not exactly like an array but works like one in this context
 
 //we can loop through the NodeList to do something  with the buttons
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   console.log(btnsOpenModal[i].textContent);
+// } //this shows the textcontent of each showModal
+
+//manipulating classes with JS
+//first thing to do is to react to a click . we will use the for loop to to do that
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
-} //this shows the textcontent of each showModal
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden'); //clasList property has a lot of methods but we will use .remove We do not use the dot here. we can also add classes and even check if an element contains a certain class
+  }); //we attached the function to the three buttons because we did it in the loop while we were looping over the Nodelist which contains all of the buttons
+}
