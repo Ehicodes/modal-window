6d09eab140a195f1cda55b9e463940d0500c925c
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-12 15:21:41 +0100

    handling key press modal and using the .contains method on classList

--- starter/script.js
+++ starter/script.js
@@ -8,8 +8,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal); //notice that it only selects one button and that is because when we use querySelector with a selector that matches multiple elements, only the first one will get selected. So instead we can use querySelectorAll
-//we stored the selected elements in the variables. the NodeList is a little bit like an array not exactly like an array but works like one in this context
+console.log(btnsOpenModal);
 
 //we can loop through the NodeList to do something  with the buttons
 // for (let i = 0; i < btnsOpenModal.length; i++) {
@@ -19,15 +18,15 @@ console.log(btnsOpenModal); //notice that it only selects one button and that is
 //manipulating classes with JS
 //first thing to do is to react to a click . we will use the for loop to to do that
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    //using classList and .remove method
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-    //clasList property has a lot of methods but we will use .remove We do not use the dot here. we can also add classes and even check if an element contains a certain class. we can remove multiple classes just by using a comma
-  }); //we attached the function to the three buttons because we did it in the loop while we were looping over the Nodelist which contains all of the buttons
-}
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     console.log('Button Clicked');
+//     //using classList and .remove method
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//
+//   }); //we attached the function to the three buttons because we did it in the loop while we were looping over the Nodelist which contains all of the buttons
+// }
 
 //closing the modal
 // btnCloseModal.addEventListener('click', function () {
@@ -36,15 +35,39 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 //   overlay.classList.add('hidden');
 // });
 
-//closing the modal when you click on the overlay or the "x" button, and using the DRY principle because we do not want the same code in overlay and btnCloseModal. so we exported that code into a more real names function
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+//closing the modal when you click on the overlay or the "x" button
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+//we specified the closeModal function
 
-btnCloseModal.addEventListener('click', closeModal); //we specified the closeModal function
+//lets do the same for the opening of the modal
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+//specified the openModal function
+
+//HOW TO HANDLE A KEY PRESS MODAL
+//-WE CAN ALSO USE THE ESC KEY TO CLOSE THE MODAL, THAT IS A THIRD WAY
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
